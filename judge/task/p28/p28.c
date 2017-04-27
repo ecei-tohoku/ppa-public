20db9fb2e7217ba7bccc403c48b2b61bd8d6437f
Naoya Inoue <naoya-i@ecei.tohoku.ac.jp>
2017-04-27 15:43:16 +0100

    Added gold standard p28.c.

--- judge/task/p28/p28.c
+++ judge/task/p28/p28.c
@@ -1,98 +1,116 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-void matrixsort(int **data, int N, int M, int ind, int rc, int order);
+int num_past_diag;
+int num_attr;
 
-int main() {
-  
-  int i, j, N, M, ind, rc, order;
-  int **data;
-  
-  scanf("%d %d %d", &ind, &rc, &order);
-  scanf("%d %d", &N, &M);
-  
-  data = (int **)malloc(N * sizeof(int*));
-  if (data==NULL) {
-    printf("Can not allocate memory. 'data' is NULL.\n");
-    return 1;
-  }
-  for (i=0; i<N; i++) {
-    data[i] = (int *)malloc(M * sizeof(int));
-  }
-  
-  for (i=0; i<M; i++) {
-    for (j=0; j<N; j++) {
-      scanf("%d",&data[i][j]);
-    }
+int *past_diag_class;
+double **past_diag_attr;
+
+double eu_dist(double *X, double *Y, int n) {
+  double ret = 0.0;
+
+  for(int i=0; i<n; i++) {
+    ret += (X[i] - Y[i])*(X[i] - Y[i]);
   }
 
-  matrixsort(data,N,M,(ind-1),rc,order);
-  
-  for (i=0; i<N; i++) {
-    for (j=0; j<M-1; j++) {
-      printf("%d ",data[i][j]);
-    }
-    printf("%d\n",data[i][M-1]);
-  }
-  
-  for (i=0; i<N; i++) {
-    free(data[i]);
-  }
-  free(data);
-  
-  return 0;
+  return sqrt(ret);
 }
 
-void matrixsort(int **data, int N, int M, int ind, int rc, int order) {
+int vote(int *X, int n) {
+  int votes[2];
 
-  int i, j, k, tmp;
-  
-  if (rc==1) {
-    for (i=0; i<M-1; i++) {
-      for (j=i+1; j<M; j++) {
-	if (order==1) {
-	  if (data[ind][i]>data[ind][j]) {
-	    for (k=0; k<N; k++) {
-	      tmp = data[k][i];
-	      data[k][i] = data[k][j];
-	      data[k][j] = tmp;
-	    }
-	  }
-	}
-	else {
-	  if (data[ind][i]<data[ind][j]) {
-	    for (k=0; k<N; k++) {
-	      tmp = data[k][i];
-	      data[k][i] = data[k][j];
-	      data[k][j] = tmp;
-	    }
-	  }
-	}	  
-      }
-    }
+  for(int i=0; i<n; i++) {
+    votes[X[i]]++;
   }
-  else {
-    for (i=0; i<N-1; i++) {
-      for (j=i+1; j<N; j++) {
-	if (order==1) {
-	  if (data[i][ind]>data[j][ind]) {
-	    for (k=0; k<M; k++) {
-	      tmp = data[i][k];
-	      data[i][k] = data[j][k];
-	      data[j][k] = tmp;
-	    }
-	  }
-	}
-	else {
-	  if (data[i][ind]<data[j][ind]) {
-	    for (k=0; k<M; k++) {
-	      tmp = data[i][k];
-	      data[i][k] = data[j][k];
-	      data[j][k] = tmp;
-	    }
-	  }
-	}	  
-      }
+
+  return votes[0] > votes[1] ? 0 : 1;
+}
+
+void sort_by(double *X, int *Y, int n) {
+  for(int i=0; i<n-1; i++) {
+    int minimum_idx = i;
+
+    for(int j=i+1; j<n; j++) {
+      if(X[minimum_idx] > X[j]) minimum_idx = j;
     }
+
+    // Swap the values.
+    double tmp_X;
+    int tmp_Y;
+    tmp_X = X[i];
+    tmp_Y = Y[i];
+    X[i] = X[minimum_idx];
+    Y[i] = Y[minimum_idx];
+    X[minimum_idx] = tmp_X;
+    Y[minimum_idx] = tmp_Y;
   }
+}
+
+int knn_diag(double *P, int k) {
+  int *votes_class = malloc(sizeof(int) * num_past_diag);
+  double *votes_dist = malloc(sizeof(double) * num_past_diag);
+
+  for(int i=0; i<num_past_diag; i++) {
+    votes_class[i] = past_diag_class[i];
+    votes_dist[i] = eu_dist(past_diag_attr[i], P, num_attr);
+  }
+
+  sort_by(votes_dist, votes_class, num_past_diag);
+
+  for(int i=0; i<k; i++) {
+    printf("%lf %d\n", votes_dist[i], votes_class[i]);
+  }
+
+  int c = vote(votes_class, k);
+
+  free(votes_class);
+  free(votes_dist);
+
+  return c;
+}
+
+int main() {
+  scanf("%d", &num_past_diag);
+  scanf("%d", &num_attr);
+
+  // Read the past diagnosis data.
+  past_diag_class = malloc(sizeof(int) * num_past_diag);
+  past_diag_attr  = malloc(sizeof(double*) * num_past_diag);
+
+  for(int i=0; i<num_past_diag; i++) {
+    past_diag_attr[i] = malloc(sizeof(double) * num_attr);
+
+    for(int j=0; j<num_attr; j++) {
+      scanf("%lf", &past_diag_attr[i][j]);
+    }
+
+    scanf("%d", &past_diag_class[i]);
+  }
+
+  // Read testing data.
+  double *test_diag_attr;
+
+  test_diag_attr = malloc(sizeof(double) * num_attr);
+
+  for(int j=0; j<num_attr; j++) {
+    scanf("%lf", &test_diag_attr[j]);
+  }
+
+  int c = knn_diag(test_diag_attr, 3);
+
+  if(c == 0) printf("You are healthy!\n");
+  else       printf("You have a heart disease!\n");
+
+  // Release the array.
+  free(past_diag_class);
+
+  for(int i=0; i<num_past_diag; i++) {
+    free(past_diag_attr[i]);
+  }
+
+  free(past_diag_attr);
+  free(test_diag_attr);
 }
