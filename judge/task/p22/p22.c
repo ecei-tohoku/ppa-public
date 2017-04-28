dac6056e3fc2cab69ed9cba5b9c06de7611a1e11
xskataoka <xkataoka@smapip.is.tohoku.ac.jp>
2017-04-29 05:03:31 +0900

    Add files via upload

--- judge/task/p22/p22.c
+++ judge/task/p22/p22.c
@@ -1,51 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int selectionsort(int *data, int N);
-
-int main() {
-  
-  int i, N, cnt;
-  int *data;
-  
-  scanf("%d",&N);
-  
-  data = (int*)malloc(N * sizeof(int));
-  if (data==NULL) {
-    printf("Can not allocate memory. 'data' is NULL.\n");
-    return 1;
-  }
-  
-  for (i=0; i<N; i++) {
-    scanf("%d",&data[i]);
-  }
-  
-  cnt = selectionsort(data,N);
-  
-  for (i=0; i<N-1; i++) {
-    printf("%d ", data[i]);
-  }
-  printf("%d\n",data[N-1]);
-  printf("%d\n",cnt);
-  
-  free(data);
-  
-  return 0;
+void printError ( void )
+{
+  printf ( "ERROR\n" );
+  exit ( EXIT_FAILURE );
 }
 
-int selectionsort(int *data, int N) {
-  int i, j, tmp, min, cnt=0;
-  for (i = 0; i < N - 1; i++) {
-    min = i;
-    for (j = i + 1; j < N; j++) {
-      cnt++;
-      if (data[min] > data[j]) {
-	min = j;
-      }
+void swap ( int *array, int a, int b )
+{
+  int tmp;
+
+  tmp = array[ a ];
+  array[ a ] = array[ b ];
+  array[ b ] = tmp;
+}
+
+void show ( int *array, int num )
+{
+  int i;
+  for ( i = 0; i < num-1; i++ )
+    {
+      printf ( "%d ", array[ i ] );
     }
-    tmp = data[i];
-    data[i] = data[min];
-    data[min] = tmp;
-  }
-  return cnt;
+  printf ( "%d\n", array[ num-1 ] );
+}
+
+int selectionsort ( int *array, int num )
+{
+  int count = 0, tmp;
+  int i, j;
+
+  for ( i = 0; i < num - 1; i++ )
+    {
+      tmp = i;
+      for ( j = i + 1; j < num; j++ )
+	{
+	  count++;
+	  if ( array[ tmp ] > array[ j ] )
+	    {
+	      tmp = j;
+	    }
+	}
+
+      if ( tmp != i )
+	{
+	  printf ( "SWAP(%d,%d)\n# ", i, tmp );
+	  swap ( array, i, tmp );
+	  show ( array, num );
+	}
+    }
+
+  return count;
+}
+
+int main ( int argc, char *argv[] )
+{
+  int num, tmp, count;
+  int *data;
+  
+  /* データの個数を読み込む */
+  if ( scanf ( "%d", &num ) == EOF )
+    {
+      printError();
+    }
+
+  /* データを読み込む */
+  if ( ( data = ( int * ) malloc ( num * sizeof ( int ) ) ) == NULL )
+    {
+      printError();
+    }
+  tmp = 0;
+  while ( ( scanf ( "%d", data + tmp ) != EOF ) || tmp < num )
+    {
+      tmp++;
+    }
+
+  /* 表示する */
+  // show ( data, num );
+
+  /* 並べ替える */
+  count = selectionsort ( data, num );
+
+  /* 表示する */
+  show ( data, num );
+  printf ( "%d\n", count );
+
+  free ( data );
+
+  return 0;
 }
