09133895e11fbd77058668d4b4597f741fe0772a
xskataoka <xkataoka@smapip.is.tohoku.ac.jp>
2017-05-02 19:31:09 +0900

    Add files via upload

--- judge/task/p21/p21.c
+++ judge/task/p21/p21.c
@@ -1,63 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void histogram(int *data, int N);
-
-int main() {
-
-  int i, N;
-  float *data_f;
-  int *data;
-  
-  scanf("%d",&N);
-  
-  data_f = (float*)malloc(N * sizeof(float));
-  if (data_f==NULL) {
-    printf("Can not allocate memory. 'data_f' is NULL.\n");
-    return 1;
-  }
-  data = (int*)malloc(N * sizeof(int));
-  if (data==NULL) {
-    printf("Can not allocate memory. 'data' is NULL.\n");
-    return 1;
-  }
-  
-  for (i=0; i<N; i++) {
-    scanf("%f",&data_f[i]);
-  }
-  for (i=0; i<N; i++) {
-    data[i] = (int)(data_f[i]+0.5);
-  }
-
-  histogram(data,N);
-  
-  free(data_f);
-  free(data);
-
-  return 0;
+void printError ( void )
+{
+  printf ( "ERROR\n" );
+  exit ( EXIT_FAILURE );
 }
 
-void histogram(int *data, int N) {
-  int MIN, MAX, i, j;
+int count ( double *array, int num, int rank )
+{
+  int count = 0, i;
+
+  for ( i = 0; i < num; i++ )
+    {
+      if ( ( int ) ( array[ i ] + 0.5) == rank )
+	{
+	  count++;
+	}
+    }
   
-  MIN = data[0];
-  MAX = data[0];
-  for (i=1; i<N; i++) {
-    if (data[i]<MIN) {
-      MIN = data[i];
+  return count;
+}
+
+int main ( int argc, char *argv[] )
+{
+  int num, tmp, i, j;
+  double *data;
+
+  // データの個数を読み込む
+  if ( scanf ( "%d", &num ) == EOF )
+    {
+      printError();
     }
-    else if (data[i]>MAX) {
-      MAX = data[i];
+
+  // データを読み込む
+  tmp = 0;
+  if ( ( data = ( double * ) malloc ( num * sizeof ( double ) ) ) == NULL )
+    {
+      printError();
     }
-  }
+  while ( ( scanf ( "%lf", data + tmp ) != EOF ) || tmp < num )
+    {
+      tmp++;
+    }
+
+  // ヒストグラムをプロットする
+  for ( i = 0; i < 21; i++ )
+    {
+      printf ( "%2d ", i );
+      tmp = count ( data, num, i );
+      for ( j = 0; j < tmp; j++ )
+	{
+	  printf ( "*" );
+	}
+      printf ( "\n" );
+    }
+
+  free ( data );
   
-  for (i=MIN; i<=MAX; i++) {
-    printf("%2d ",i);
-    for (j=0; j<N; j++) {
-      if (data[j]==i) {
-	printf("*");
-      }
-    }
-    printf("\n");
-  }
+  return 0;
 }
