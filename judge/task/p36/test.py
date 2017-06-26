d5d67e4215dd27f608851b25692828552e917d08
Naoya Inoue <naoya-i@ecei.tohoku.ac.jp>
2017-06-26 14:20:08 +0900

    Improved p36 tester.

--- judge/task/p36/test.py
+++ judge/task/p36/test.py
@@ -7,16 +7,42 @@ DR = d['stdout_gold']
 
 check = DT == DR
 
-#
 # Special treatment for t05 (ugly solution. To be modified next year).
 if DR == """MHPL ILAFVGAAAVAFPSDY
 MNPLLILTFV AALAAF DDI
 =R==I==R==D==RR==DR=R
 """:
-    DR2 = """MHP LILAFVGAAAVAFPSDY
-MNPLLILTFV AALAAF DDI
-=R=I===R==D==RR==DR=R
-"""
-    check |= DT == DR2
+    DT = DT.split("\n")
 
+    # Is the output well-formed?
+    assert(len(DT) == 4)
+
+    src, dest, ops, _ = DT
+
+    # The number of edit operations should be 9.
+    assert(len(list(filter(lambda x: x!="=", ops))) == 9)
+    
+    src = src.ljust(len(ops))
+    dest = dest.ljust(len(ops))
+
+    for i in range(len(ops)):
+        judge = False
+
+        if ops[i] == "=" and src[i] == dest[i]:
+            judge = True
+            
+        if ops[i] == "D" and src[i] != " " and dest[i] == " ":
+            judge = True
+            
+        if ops[i] == "I" and src[i] == " " and dest[i] != " ":
+            judge = True
+            
+        if ops[i] == "R" and src[i] != " " and dest[i] != " " and src[i] != dest[i]:
+            judge = True
+
+        if not judge:
+            sys.exit(1)
+
+    sys.exit(0)
+    
 sys.exit(0 if check else 1)
