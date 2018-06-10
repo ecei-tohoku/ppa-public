f25540884dbf8465172558b807ce6896808aa904
Naoya Inoue <naoya-i@ecei.tohoku.ac.jp>
2018-06-10 21:51:50 +0900

    Add files via upload

--- judge/task/p37/p37.c
+++ judge/task/p37/p37.c
@@ -1,76 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-
-int max(int x, int y) { return x > y ? x : y; };
-int min(int x, int y) { return x < y ? x : y; };
-
-int min3(int a, int b, int c) {
-  return min(a, min(b, c));
-}
-
-int ld_dp(int d[100][100], char *x, int m, char *y, int n) {
-  int i, j;
-
-  /* Fill c_{0,i} and c_{j,0}. */
-  for(i=0; i<m+1; i++) d[i][0] = i;
-  for(j=0; j<n+1; j++) d[0][j] = j;
-
-  for(i=1; i<m+1; i++) {
-    for(j=1; j<n+1; j++) {
-      d[i][j] = min3(
-        d[i-1][j-1] + (x[i-1] != y[j-1] ? 1 : 0),
-        d[i-1][j] + 1,
-        d[i][j-1] + 1
-      );
-    }
-  }
-
-  return d[m][n];
-}
+int dp(char *x, int Lx, char *y, int Ly, int **ed);
+int min3(int a, int b, int c);
+int d(char x, char y);
 
 int main() {
-  char w[100];
-  int  i;
-  int  size_vocab;
-  int  dm[100][100];
-  char dict[5000][100];
-  int  edist[5000];
 
-  // Read a dictionary.
-  scanf("%d", &size_vocab);
+  char x[25], **dict;
+  int i, j, Lx, Ly, N, *len, min_id[3];
+  int **ed;
 
-  for(i=0; i<size_vocab; i++) {
-    scanf("%s", dict[i]);
-  }
+  scanf("%d",&N);
 
-  // Wait for the user input.
-  while(scanf("%s", w) != EOF) {
-    int min_w = 0, out_w = 0;
+  dict = (char **)malloc(sizeof(char *)*N);
+  for (i=0; i<N; i++) dict[i] = (char *)malloc(sizeof(char)*25);
+  for (i=0; i<N; i++) scanf("%s",dict[i]);
 
-    // Calculate edit distance between the input word and candidates.
-    for(i=0; i<size_vocab; i++) {
-      edist[i] = ld_dp(dm, dict[i], strlen(dict[i]), w, strlen(w));
+  len = (int *)malloc(sizeof(int)*N);
+  
+  while (scanf("%s",x)!=EOF) {
+    Lx = strlen(x);
+    for (i=0; i<3; i++) min_id[i] = -1;
+    for (i=0; i<N; i++) {
+      Ly = strlen(dict[i]);
 
-      if(edist[min_w] > edist[i]) {
-        min_w = i;
+      ed = (int **)malloc(sizeof(int *)*(Lx+1));
+      for (j=0; j<Lx+1; j++) ed[j] = (int *)malloc(sizeof(int)*(Ly+1));
+
+      len[i] = dp(x,Lx,dict[i],Ly,ed);
+      
+      if (min_id[0]==-1||len[min_id[0]]>len[i]) {
+	min_id[2] = min_id[1];
+	min_id[1] = min_id[0];
+	min_id[0] = i;
       }
+      else if (min_id[1]==-1||len[min_id[1]]>len[i]) {
+	min_id[2] = min_id[1];
+	min_id[1] = i;
+      }
+      else if (min_id[2]==-1||len[min_id[2]]>len[i]) min_id[2] = i;
+	
+      for (j=0; j<Lx+1; j++) free(ed[j]);
+      free(ed);
     }
 
-    // Output minimum edit distance words.
-    for(i=0; i<size_vocab; i++) {
-      if(edist[i] == edist[min_w]) {
-        if(out_w > 0)
-          printf(" ");
-
-        printf("%s", dict[i]);
-        out_w++;
-      }
+    for (i=0; i<3; i++) {
+      printf("rank: %d, ed = %d, string = %s\n",i+1,len[min_id[i]],dict[min_id[i]]);
     }
-
-    printf("\n");
+    //for (i=0; i<Lx+1; i++) free(ed[i]);
+    //free(ed);
+    
   }
-
+  
+  for (i=0; i<N; i++) free(dict[i]);
+  free(dict);
+  free(len);
+  
   return 0;
+}
+
+int dp(char *x, int Lx, char *y, int Ly, int **ed) {
+  int i, j;
+  
+  for (j=0; j<Lx+1; j++) ed[j][0] = j;
+  for (j=0; j<Ly+1; j++) ed[0][j] = j;
+  
+  for (i=1; i<Lx+1; i++) {
+    for (j=1; j<Ly+1; j++) {
+      ed[i][j] = min3(ed[i-1][j-1]+d(x[i-1],y[j-1]),
+		      ed[i-1][j]+1,
+		      ed[i][j-1]+1);
+    }
+  }
+  return ed[Lx][Ly];
+}
+
+int min3(int a, int b, int c) {
+  int min;
+  min = a;
+  if (b<min) min = b;
+  if (c<min) min = c;
+  return min;
+}
+
+int d(char x, char y) {
+  return (x==y) ? 0 : 1;
 }
