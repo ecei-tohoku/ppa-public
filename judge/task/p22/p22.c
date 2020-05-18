a8dabbd12ef5d0a1bbc52f2c7553b9832e56f7d4
TiagoGR92 <63337530+TiagoGR92@users.noreply.github.com>
2020-05-18 13:40:15 +0900

    Added answer and test cases

--- judge/task/p22/p22.c
+++ judge/task/p22/p22.c
@@ -1,92 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printError ( void )
-{
-  printf ( "ERROR\n" );
-  exit ( EXIT_FAILURE );
+int* func_swap_(int* const array, const int pos_a,const int pos_b) {
+  int tmp = array[pos_a];
+  array[pos_a] = array[pos_b];
+  array[pos_b] = tmp;
+  printf("SWAP(%2d,%2d)", pos_a, pos_b);
+  return array;
 }
 
-void swap ( int *array, int a, int b )
-{
-  int tmp;
-
-  tmp = array[ a ];
-  array[ a ] = array[ b ];
-  array[ b ] = tmp;
+int comp_(int* const array, const int pos_a, const int pos_b) {
+  if (array[pos_a] < array[pos_b]) return 1;
+  return 0;
 }
 
-void show ( int *array, int num )
-{
-  int i;
-  for ( i = 0; i < num-1; i++ )
-    {
-      printf ( "%d ", array[ i ] );
-    }
-  printf ( "%d\n", array[ num-1 ] );
+void print_array_(const int* const array,const int N) {
+  int ri = 0;
+  for (; ri < N; ++ri) {
+    printf(" %2d", array[ri]);
+  }
+  printf("\n");
 }
 
-int selectionsort ( int *array, int num )
-{
-  int count = 0, tmp;
-  int i, j;
-
-  for ( i = 0; i < num - 1; i++ )
-    {
-      tmp = i;
-      for ( j = i + 1; j < num; j++ )
-	{
-	  count++;
-	  if ( array[ tmp ] > array[ j ] )
-	    {
-	      tmp = j;
-	    }
-	}
-
-      if ( tmp != i )
-	{
-	  printf ( "SWAP(%d,%d)\n# ", i, tmp );
-	  swap ( array, i, tmp );
-	  show ( array, num );
-	}
-    }
-
-  return count;
+int* func_mallocation_(const int N) {
+  int* data = (int*)malloc(N * sizeof(int));
+  if (data == NULL) {
+    printf("Can not allocate memory. 'data' is NULL.\n");
+    exit(EXIT_FAILURE);
+  }
+  return data;
 }
 
-int main ( int argc, char *argv[] )
-{
-  int num, tmp, count;
-  int *data;
-  
-  /* データの個数を読み込む */
-  if ( scanf ( "%d", &num ) == EOF )
-    {
-      printError();
-    }
 
-  /* データを読み込む */
-  if ( ( data = ( int * ) malloc ( num * sizeof ( int ) ) ) == NULL )
-    {
-      printError();
-    }
-  tmp = 0;
-  while ( ( scanf ( "%d", data + tmp ) != EOF ) || tmp < num )
-    {
-      tmp++;
-    }
 
-  /* 表示する */
-  // show ( data, num );
+int bub_sort(int *, int);
 
-  /* 並べ替える */
-  count = selectionsort ( data, num );
+int main() {
 
-  /* 表示する */
-  show ( data, num );
-  printf ( "%d\n", count );
+  int N, *array, i;
 
-  free ( data );
+  scanf("%d", &N);
+  array = func_mallocation_(N);
+  for(i=0; i<N; i++) scanf("%d", &(array[i]));
+  printf("INIT:::::::"); print_array_(array, N);
+  i=bub_sort(array, N);
+  printf("DONE:::::::"); print_array_(array, N);
+  printf("%d\n", i);
+
+  free(array);
 
   return 0;
+}
+
+int bub_sort(int *array, int N) {
+
+  int cur_pos, end_pos, comp_count;
+
+  comp_count = 0;
+  for(end_pos=N-1; end_pos>0; end_pos--) {
+    for(cur_pos=0; cur_pos<end_pos; cur_pos++) {
+        comp_count++;
+        if(comp_(array, cur_pos+1, cur_pos)) {
+            func_swap_(array, cur_pos, cur_pos+1);
+            print_array_(array, N);
+        }
+    }
+  }
+
+
+  return comp_count;
 }
